c7377051ae3d604fa46115146df27a6babd06443
sangelone <angelone@gmail.com>
2010-05-09 18:37:36 -0500

    Created a package

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='python-hashes',
+      version='0.1',
+      description='Library of interesting (non-cryptographic) hashes in pure Python.',
+      author='sangelone',
+      author_email='angelone@gmail.com',
+      url='http://github.com/sangelone/python-hashes',
+      download_url='http://github.com/sangelone/python-hashes/downloads',
+      packages=['hashes'],
+      )
