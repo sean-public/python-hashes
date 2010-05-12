feac6ee97417bc04f06ae4fea5003725f49c0184
sangelone <angelone@gmail.com>
2010-05-11 23:51:38 -0500

    Created a base class for hash types and implemented simhashes using it.

--- hashes/__init__.py
+++ hashes/__init__.py
@@ -1,2 +1,3 @@
 #!/usr/bin/env python
 
+"Interesting (non-cryptographic) hashes implemented in pure Python."
--- hashes/simhash.py
+++ hashes/simhash.py
@@ -1,37 +1,27 @@
-#!/usr/bin/env python
+"""
+Implementation of Charikar similarity hashes in Python.
 
-# Implementation of Charikar simhashes in Python
-# Reference used: http://dsrg.mff.cuni.cz/~holub/sw/shash
+Most useful for creating 'fingerprints' of documents or metadata
+so you can quickly find duplicates or cluster items.
 
-# This implementation by sangelone:
-# http://github.com/sangelone/python-hashes
+Part of python-hashes by sangelone. See README for details and license.
+"""
 
-class simhash():
-    def __init__(self, tokens='', hashbits=96):
-        self.hashbits = hashbits
-        self.hash = self.simhash(tokens)
+from hashtype import hashtype
 
-    def __trunc__(self):
-        return self.hash
-
-    def __str__(self):
-        return str(self.hash)
-    
-    def __long__(self):
-        return long(self.hash)
-
-    def __float__(self):
-        return float(self.hash)
+class simhash(hashtype):
+    def hash(self, tokens):
+        """Returns a Charikar simhash with appropriate bitlength.
         
-    def __cmp__(self, other):
-        if self.hash < long(other): return -1
-        if self.hash > long(other): return 1
-        return 0
-    
-    def simhash(self, tokens):
-        # Returns a Charikar simhash with appropriate bitlength
-        v = [0]*self.hashbits
-    
+        Input can be any iterable, but for strings it will automatically
+        break it into words first, assuming you don't want to iterate
+        over the individual characters.
+        
+        Reference used: http://dsrg.mff.cuni.cz/~holub/sw/shash
+        """
+        if type(tokens) == str:
+            tokens = tokens.split()
+        v = [0]*self.hashbits    
         for t in [self._string_hash(x) for x in tokens]:
             bitmask = 0
             for i in xrange(self.hashbits):
@@ -40,16 +30,15 @@ class simhash():
                     v[i] += 1
                 else:
                     v[i] -= 1
-    
+
         fingerprint = 0
         for i in xrange(self.hashbits):
             if v[i] >= 0:
-                fingerprint += 1 << i
-        
+                fingerprint += 1 << i        
         return fingerprint
 
     def _string_hash(self, v):
-        # A variable-length version of Python's builtin hash
+        "A variable-length version of Python's builtin hash. Neat!"
         if v == "":
             return 0
         else:
@@ -63,31 +52,13 @@ class simhash():
                 x = -2
             return x
 
-    def hamming_distance(self, other_hash):
-        x = (self.hash ^ other_hash.hash) & ((1 << self.hashbits) - 1)
-        tot = 0
-        while x:
-            tot += 1
-            x &= x-1
-        return tot
-
     def similarity(self, other_hash):
+        """Calculate how different this hash is from another simhash.
+        Returns a float from 0.0 to 1.0 (inclusive)
+        """
+        if type(other_hash) != simhash:
+            raise Exception('Hashes must be of same type to find similarity')
         b = self.hashbits
         if b!= other_hash.hashbits:
-            raise Exception('Hashes must be of equal size to find similarity!')
+            raise Exception('Hashes must be of equal size to find similarity')
         return float(b - self.hamming_distance(other_hash)) / b
-
-
-if __name__ == '__main__':
-    s1 = 'This is a test string for tesing 12132131'
-    s2 = 'PubMed comprises more than 19 million citations'
-    #s2 = 'This is a test string for testing also!'
-
-    for bits in xrange(16, 100, 16):
-        hash1 = simhash(s1.split(), bits)
-        hash2 = simhash(s2.split(), bits)
-
-        print "%s\t[simhash = 0x%x]" % (s1, hash1)
-        print "%s\t[simhash = 0x%x]" % (s2, hash2)
-        print "%.3f%% similar because" % (hash1.similarity(hash2)*100),
-        print hash1.hamming_distance(hash2), "bits differ out of", hash1.hashbits
